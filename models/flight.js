14dfceeab5ce28281e1a1dbeccf5136d3e5a2665
Shiva Shrestha <shivashrestha927@gmail.com>
2022-07-16 17:43:09 +0545

    Fetch Flight

--- models/flight.js
+++ models/flight.js
@@ -2,13 +2,13 @@ const db = require('../util/database');
 
 module.exports = class flight{
     static fetchAll() {
-        return db.execute('SELECT * FROM FLIGHT NATURAL JOIN ROUTE');
+        return db.execute("SELECT * FROM FLIGHT NATURAL JOIN ROUTE");
     }
     static fetchFlightinfo(s, d, depDate, qty){
         return db.execute("SELECT * FROM FLIGHT NATURAL JOIN ROUTE WHERE origin = '"+s+"'AND destination = '"+d+"'AND Date = '"+depDate+"' AND empty_seats>="+qty);
     }
     static fetchFlightSeatinfo(id)
     {
-        return db.execute("SELECT * FROM FLIGHT NATURAL JOIN SEAT")
+        return db.execute("SELECT * FROM FLIGHT NATURAL JOIN SEAT F_ID = "+id);
     }
 }
